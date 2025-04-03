1b0cc634cec31f939b557e0968e0de7d88af4aa7
Abril Diaco <adiaco@udesa.edu.ar>
2025-04-03 20:52:53 -0300

    j

--- Ej1/empresa.cpp
+++ Ej1/empresa.cpp
@@ -6,42 +6,6 @@
 
 using namespace std;
 
-// //CLASE ABSTRACTA
-// class EntidadOrganizativa{
-//     protected: //PREGUNTAR
-//         std::string nombre;
-//         std::vector<unique_ptr<EntidadOrganizativa>> subentidades;
-    
-//     public:
-//         EntidadOrganizativa(std::string, std::vector<unique_ptr<EntidadOrganizativa>>);
-//         virtual std::string getNombre() const;
-//         virtual void agrgarSubentidad(unique_ptr<EntidadOrganizativa>) = 0;
-//         virtual int contarSubentidades()const;
-//         virtual ~EntidadOrganizativa();    
-// };       
-
-// //CLASES DERIVADAS
-
-// class Empresa: public EntidadOrganizativa{
-//     private:
-//         std::vector<std::Departamento> departamentos;
-    
-//     public:
-//         //constructor
-//         Empresa(std::string, std::string);//falta algo
-
-//         //métodos a sobreescribir
-//         std::string getNombre() const override;
-//         void agrgarSubentidad(unique_ptr<EntidadOrganizativa>)const override;
-//         int contarSubentidades() const override;
-
-//         //métodos/atributos propios
-//         //std::string nombre; no hace falta esta en la general
-//         std::string direccion;
-//         Departamento getDepByName(std::string) const;
-//         std::vector<Departamento> getDepNames() const;
-// };
-
 //constructor clase abstracta
 
 EntidadOrganizativa::EntidadOrganizativa(string unNombre) //VER DONDE PONER EL CONSTRUCTOR
