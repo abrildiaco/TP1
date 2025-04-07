a58decd9adaac11bcd096f288cb9dee43fb27ec9
Abril Diaco <adiaco@udesa.edu.ar>
2025-04-07 19:05:53 -0300

    algo

--- Ej1/dep.cpp
+++ Ej1/dep.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <memory>
 #include <iostream>
-#include <stdexcept>+
+#include <stdexcept>
 
 using namespace std;
     
---
+++ Ej1/main1.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "personal.h"
+#include "entidades.h"
+#include "departamento.h"
+
+using namespace std;
+
+int main(){
+    //contador de empleados de todos dept
+
+    cout<<"=== Generando empleados ==="<<endl;
+
+}
